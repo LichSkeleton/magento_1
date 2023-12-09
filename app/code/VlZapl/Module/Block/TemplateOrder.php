df5f2256e832ba0d485954861d0c14355f1e5060
LichSkeleton <lichskeleton1@gmail.com>
2023-12-09 17:18:09 +0200

    Handles, Modules, Catalogs. Templates little

---
+++ app/code/VlZapl/Module/Block/TemplateOrder.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace VlZapl\Module\Block;
+
+class TemplateOrder extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'VlZapl_Module::default_custom.phtml';
+}
